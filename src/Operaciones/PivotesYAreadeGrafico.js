387360768e748e604ebb0bd78073cd33b2cbf001
Wartaron <plazasdanilo@hotmail.com>
2018-07-10 22:11:07 -0500

    Modificacion Graficas Ya se puede garficar la informacion

--- src/Operaciones/PivotesYAreadeGrafico.js
+++ src/Operaciones/PivotesYAreadeGrafico.js
@@ -54,29 +54,124 @@ class Pivotes_Area_Grafico extends Component {
 
   graficar(e){
     e.preventDefault();
+    d3.select("g").remove();
     var datos = this.devolverDatosSegunPivot();
 
     var n = 1; //Numero de series (Periodos)
     var m = datos[0].length - 3; //Numero de valores por serie
 
-    var xz = d3.range(m),
-        yz = datos[1].filter((dato, i) =>{
-          return i > 2;
-        });
+    var xz = datos[0].filter((dato, i) =>{
+                              return i > 2;
+                            });
+    var yz = datos[1].filter((dato, i) =>{
+                              return i > 2;
+                            });
 
-        yz = yz.map((dato, i) =>{
-          return parseFloat(dato);
-        });
 
-    var y01z = d3.stack().keys(d3.range(n))(d3.transpose(yz)),
-        yMax = d3.max(yz, function(y) { return d3.max(y); }),
-        y1Max = d3.max(y01z, function(y) { return d3.max(y, function(d) { return d[1]; }); });
+    yz = yz.map((dato, i) =>{
+      return parseFloat(dato);
+    });
+
+    yz = [yz];
+
+    var y01z = d3.stack().keys(d3.range(n))(d3.transpose(yz));
+    var yMax = d3.max(yz, function(y) { return d3.max(y); });
+    var y1Max = d3.max(y01z, function(y) { return d3.max(y, function(d) { return d[1]; }); });
 
     console.log(xz);
     console.log(yz);
     console.log(y01z);
     console.log(yMax);
     console.log(y1Max);
+
+
+    var svg = d3.select("svg"),
+        margin = {top: 40, right: 10, bottom: 20, left: 10},
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom,
+        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scaleBand()
+        .domain(d3.range(m))
+        //.tickValues(xz)
+        .rangeRound([0, width])
+        .padding(0.08);
+
+    var y = d3.scaleLinear()
+        .domain([0, y1Max])
+        .range([height, 0]);
+
+    var color = d3.scaleOrdinal()
+        .domain(xz)
+        .range(d3.schemeCategory10);
+
+    var series = g.selectAll(".series")
+      .data(y01z)
+      .enter().append("g")
+        .attr("fill", function(d, i) { return color(i); });
+
+    var rect = series.selectAll("rect")
+      .data(function(d) { return d; })
+      .enter().append("rect")
+        .attr("x", function(d, i) { return x(i); })
+        .attr("y", height)
+        .attr("width", x.bandwidth())
+        .attr("height", 0);
+
+    rect.transition()
+        .delay(function(d, i) { return i * 10; })
+        .attr("y", function(d) { return y(d[1]); })
+        .attr("height", function(d) { return y(d[0]) - y(d[1]); });
+
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x)
+            .tickSize(0)
+            .tickPadding(6));
+
+    d3.selectAll("input")
+        .on("change", changed);
+
+    var timeout = d3.timeout(function() {
+      d3.select("input[value=\"grouped\"]")
+          .property("checked", true)
+          .dispatch("change");
+    }, 2000);
+
+    function changed() {
+      timeout.stop();
+      if (this.value === "grouped") transitionGrouped();
+      else transitionStacked();
+    }
+
+    function transitionGrouped() {
+      y.domain([0, yMax]);
+
+      rect.transition()
+          .duration(500)
+          .delay(function(d, i) { return i * 10; })
+          .attr("x", function(d, i) { return x(i) + x.bandwidth() / n * this.parentNode.__data__.key; })
+          .attr("width", x.bandwidth() / n)
+        .transition()
+          .attr("y", function(d) { return y(d[1] - d[0]); })
+          .attr("height", function(d) { return y(0) - y(d[1] - d[0]); });
+    }
+
+    function transitionStacked() {
+      y.domain([0, y1Max]);
+
+      rect.transition()
+          .duration(500)
+          .delay(function(d, i) { return i * 10; })
+          .attr("y", function(d) { return y(d[1]); })
+          .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+        .transition()
+          .attr("x", function(d, i) { return x(i); })
+          .attr("width", x.bandwidth());
+    }
+
+
   }
 
   devolverDatosSegunPivot(){
@@ -150,7 +245,7 @@ class Pivotes_Area_Grafico extends Component {
         <div className="card mt-4">
           <form className="form" onSubmit={this.graficar}>
             <label className="form-control"><input type="radio" name="mode" value="grouped"/> Grouped</label>
-            <label className="form-control"><input type="radio" name="mode" value="stacked" /> Stacked</label>
+            <label className="form-control"><input type="radio" name="mode" value="stacked" checked /> Stacked</label>
 
             <select
               type="text"
